a5ccd3c942abc64a81d02d3cc8c2c3a9b4f1380c
Steven Blom <atomicblom@binaryvibrance.net>
2018-05-03 09:34:52 +0930

    Don't open gui while snapping

--- src/main/java/com/github/atomicblom/finishingtouch/DecalWandItem.java
+++ src/main/java/com/github/atomicblom/finishingtouch/DecalWandItem.java
@@ -1,5 +1,6 @@
 package com.github.atomicblom.finishingtouch;
 
+import com.github.atomicblom.finishingtouch.handlers.DecalPositioningHandler;
 import com.github.atomicblom.finishingtouch.handlers.GuiHandler;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
@@ -15,8 +16,10 @@ public class DecalWandItem extends Item
 	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand handIn)
 	{
 		if (player.isSneaking()) {
-			player.openGui(TheFinishingTouch.INSTANCE, GuiHandler.DECAL_SELECTOR, world, (int)player.posX, (int)player.posY, (int)player.posZ);
-			return new ActionResult<>(EnumActionResult.SUCCESS, player.getHeldItem(handIn));
+			if (world.isRemote && !DecalPositioningHandler.isPlacing()) {
+				player.openGui(TheFinishingTouch.INSTANCE, GuiHandler.DECAL_SELECTOR, world, (int) player.posX, (int) player.posY, (int) player.posZ);
+				return new ActionResult<>(EnumActionResult.SUCCESS, player.getHeldItem(handIn));
+			}
 		}
 
 		return new ActionResult<>(EnumActionResult.PASS, player.getHeldItem(handIn));
